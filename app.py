41f7edd9caf2dda88101d43849f3f28b09513a40
Hossein Nekouei <nekouei.hosein@gmail.com>
2025-07-21 15:01:39 +0330

    add process function to recieve a json request over POST method and read the text as message

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app= Flask(__name__)
 
@@ -7,10 +7,14 @@ def home():
     return "Welcome to the SMS Verification Service"
 
 
-@app.route('/v1/get_sms', methods=['POST'])
-def get_sms():
-    # Logic to initiate SMS verification
-    return "SMS verification initiated"
+@app.route('/v1/process', methods=['POST'])
+def process():
+    
+   response= request.get_json()
+   message= response["message"]
+   print(message)
+   
+   return jsonify(response),200  
 
 def send_sms(number, message):
     # Logic to send SMS
@@ -19,3 +23,6 @@ def send_sms(number, message):
 def check_serial():
     # Logic to check serial number
     return "Serial number checked"
+
+if __name__ == '__main__':
+    app.run('127.0.0.1',5000,debug=True)
