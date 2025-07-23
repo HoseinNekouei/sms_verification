34a3fc449dfc9bbec592c7b15f012e9d7b37eae5
Hossein Nekouei <nekouei.hosein@gmail.com>
2025-07-23 12:17:14 +0330

    emplemented send sms function based on provider document

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request
+import requests
+import config
 
 app= Flask(__name__)
 
@@ -16,13 +18,27 @@ def process():
    
    return jsonify(response),200  
 
-def send_sms(number, message):
-    # Logic to send SMS
-    return "SMS sent"
+def send_sms(number :str ,message :str):
+
+    data= {'user':config.user_name,
+           'pass': config.password,
+           'line': config.line,
+           'mobile': number,
+           'message': message}
+
+#    data= {'sender': config.sender,
+#        'receptor': number, 
+#        'message':message}
+    print(data)
+    response= requests.post(config.url, data=data)
+
+    print(f'response is: {response}')
+
 
 def check_serial():
     # Logic to check serial number
     return "Serial number checked"
 
 if __name__ == '__main__':
+    send_sms('09155603281','this message send via payamkotah service')
     app.run('127.0.0.1',5000,debug=True)
